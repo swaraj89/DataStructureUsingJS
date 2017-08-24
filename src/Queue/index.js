f66ba55d8902a81987766a6ddacfd7698f59ce1a
Swaraj Panigrahi <panigrahiswaraj89@gmail.com>
2017-08-24 14:26:02 +0530

    Completed Queue DS.

--- src/Queue/index.js
+++ src/Queue/index.js
@@ -7,6 +7,115 @@
 
 export default class Queue {
     constructor(){
-        
+        this.rear = null;
+        this.front = null;
+        this.size = 0;
     }
+
+    clear(){
+        this.rear = null;
+        this.front = null;
+        this.size = 0;
+    }
+
+    isEmpty(){
+        return (this.rear === null);
+    }
+
+    getHead(){
+        return this.front ? this.front.data : null;
+    }
+
+    peekAt(loc = 0 ){
+        if(loc < this.size){
+            let crnt = this.front;
+            for (let index = 0; index < loc; index++) {
+                crnt = crnt.next;
+            }
+
+            return crnt.data;
+        }
+
+        return -1;
+    }
+
+    enQueue(data){
+
+        let node = {
+            data: data,
+            next: null
+        };
+
+        if(this.front === null){
+            this.front = this.rear = node;
+        }else{
+            this.rear.next = node;
+            this.rear = node;
+        }
+
+        this.size++;
+    }
+
+    deQueue(){
+        let data = null;
+
+        if(this.front){
+            data = this.front.data;
+
+            this.front = this.front.next;
+
+            if(this.front == null){
+                this.rear = null;
+            }
+            this.size--;
+        }
+    
+        return data;
+    }
+
+    *[Symbol.iterator](){
+        let current = this.front;
+
+        while(current){
+            yield current.data;
+            current = current.next;
+        }
+    }
+
+    forEach(iterator) {
+        if (typeof iterator !== 'function')
+            throw new Error('iterator should be function');
+
+        let current = this.front;
+        while (current) {
+            if (iterator(current.data)) break;
+            current = current.next;
+        }
+    }
+
+    toArray(){
+        if(this.front === null){
+            return null;
+        }
+
+        let arr = [];
+        let current = this.front;
+
+        this.forEach(function(data){
+            arr.push(data);
+        });
+
+        return arr;
+    }
+
+    toString(){
+        let arr = this.toArray();
+
+        if(arr){
+            return arr.join(",");
+        }
+
+        return "";
+    }
+
 };
--- test/Queue/queue.test.js
+++ test/Queue/queue.test.js
@@ -9,7 +9,76 @@ describe('Queue', function() {
         queue = new Queue();
     });
 
+    afterEach(function () {
+        queue.clear();
+    });
+
     it('should be able to create a instance of Queue', function() {
+        assert.instanceOf(queue, Queue, "Queue has been instantiated.")
+    });
+
+    it('should be able to enqueue', function () {
+        queue.enQueue(1);
+
+        assert.equal(queue.peekAt(0), 1);
+        assert.equal(queue.getHead(), 1);
+        assert.equal(queue.size, 1);
+
+    });
+
+    it('should be able to peek at any location', function () {
+        queue.enQueue(1);
+        assert.equal(queue.peekAt(0), 1);
+
+        queue.enQueue(2);
+        assert.equal(queue.peekAt(1), 2);
+
+        assert.equal(queue.peekAt(3), -1);
+    });
+
+    it('should be able to get the head', function () {
+        queue.enQueue(4);
+
+        assert.equal(queue.getHead(), 4);
+
+    });
+
+    it('should be able to get all the elements by toString() as csv', function () {
+        assert.equal(queue.toString(), "");
         
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(5);
+
+        assert.equal(queue.toString(), "1,2,3,4,5");
+
+    });
+
+    it('should be able to get all the elements by toArray() as an array', function () {
+        assert.equal(queue.toArray(), null);
+        
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(5);
+
+        assert.deepEqual(queue.toArray(), [1,2,3,4,5]);
+    });
+
+    it('should be able to remove elements using dequeue method', function () {
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+
+        assert.equal(queue.size, 3);
+        assert.equal(queue.deQueue(), 1);
+        assert.equal(queue.size, 2);
+
+        assert.equal(queue.size, 2);
+        assert.equal(queue.deQueue(), 2);
+        assert.equal(queue.size, 1);
     });
 });
